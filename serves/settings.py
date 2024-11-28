e76afee1dad3b41896716d2c32e617aa6e4d2580
Sem765887 <semyon.gardt765887@mail.ru>
2024-11-28 08:53:02 +0700

    Фикс бага с добавлением категории

--- serves/settings.py
+++ serves/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,19 +109,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (css, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = 'studio/account/'
+
+AUTH_USER_MODEL = 'studio.UserProfile'
--- studio/admin.py
+++ studio/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-# admin.site.register(Request)
-# admin.site.register(Category)
+admin.site.register(Request)
+admin.site.register(Category)
 admin.site.register(UserProfile)
