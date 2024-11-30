c7a38a8144d34de7b5bd1bbba247d00e3f31ebcb
Sem765887 <semyon.gardt765887@mail.ru>
2024-11-30 09:30:32 +0700

    Слайдер исправленный

--- studio/static/JS/main.js
+++ studio/static/JS/main.js
@@ -4,6 +4,6 @@ var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide() {
   slides[currentSlide].className = 'slide';
-  currentSlide = (currentSlide+1)%slides.length;
+  currentSlide = (currentSlide+1)%3;
   slides[currentSlide].className = 'slide active';
 }
